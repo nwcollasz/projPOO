cd4d50ad45ef81434b1d89aa672872928a95b052
Nicollas <Nicollas@DESKTOP-L0F4JID>
2025-08-26 12:10:07 -0300

    Adicionar arquivos de projeto.

---
+++ projPOO_ex2/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex2
+{
+    internal class Quadrado
+    {
+        private int l;
+        private int sqr;
+
+        public void setAresta(int l)
+        {
+            this.l = l;
+        }
+
+        public int getAresta(int l)
+        {
+            return this.l;
+        }
+
+        public int getArea()
+        {
+            return this.sqr;
+        }
+
+        public void calcularArea()
+        {
+            this.sqr = this.l * this.l;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
