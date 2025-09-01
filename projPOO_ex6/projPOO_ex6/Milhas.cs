2b8446ed9475e142d5b9b81c7e92bf9d78e2e330
nicollas <nicollas.b@aluno.ifsp.edu.br>
2025-09-01 19:59:52 -0300

    Add files via upload

---
+++ projPOO_ex6/projPOO_ex6/Milhas.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex6
+{
+    internal class Milhas
+    {
+        private double milha;
+        private double km;
+
+        public void setNmilha(double milha)
+        {
+            this.milha = milha;
+        }
+
+        public double getNmilha(double milha)
+        {
+            return this.milha;
+        }
+
+        public void setKm(double km)
+        {
+            this.km = km;
+        }
+
+
+
+        public double getKm()
+        {
+            return this.km;
+        }
+
+        public void calcularKm()
+        {
+            this.km = this.milha * 1.852;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ projPOO_ex6/projPOO_ex6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Milhas mil;
+            mil = new Milhas();
+
+            Console.WriteLine("Digite o valor em quilometros: ");
+            mil.setNmilha(double.Parse(Console.ReadLine()));
+            mil.calcularKm();
+            Console.WriteLine("Milhas Náuticas{0}.", mil.getKm());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
