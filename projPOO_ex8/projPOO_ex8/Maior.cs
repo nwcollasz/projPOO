cfabf41a81a0263ea35cb6a69e2488c3f2108202
nicollas <nicollas.b@aluno.ifsp.edu.br>
2025-09-01 20:44:59 -0300

    Add files via upload

---
+++ projPOO_ex8/projPOO_ex8/Maior.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex8
+{
+    internal class Maior
+    {
+
+        private int a;
+        private int b;
+        private int maior;
+        public void setA(int a)
+        {
+            this.a = a;
+        }
+
+        public int getA(int a)
+        {
+            return this.a;
+        }
+
+        public void setB(int b)
+        {
+            this.b = b;
+        }
+
+        public int getB()
+        {
+            return this.b;
+        }
+
+        public int getMaior()
+        {
+            return this.maior;
+        }
+        public void calcularAB()
+        {
+            if (this.a > this.b)
+            {
+              this.maior = this.a;
+            }
+
+            else
+            {
+                this.maior = this.b;
+            }
+
+            if (this.a == this.b)
+            {
+                Console.WriteLine("Os valores sao identicos.");
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ projPOO_ex8/projPOO_ex8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior ma;
+            ma = new Maior();
+
+            
+            Console.WriteLine("Digite um valor:");
+            ma.setA(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite outro valor:");
+            ma.setB(int.Parse(Console.ReadLine()));
+            ma.calcularAB();
+            Console.WriteLine("{0} é o maior valor", ma.getMaior());
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
