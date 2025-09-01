5a3595221a5ca0f3656732dbb2fe466b9cfd3f15
nicollas <nicollas.b@aluno.ifsp.edu.br>
2025-09-01 20:15:59 -0300

    Add files via upload

---
+++ projPOO_ex7/projPOO_ex7/Dolar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex7
+{
+    internal class Dolar
+    {
+        private double dolar;
+        private double real;
+
+        public void setDolar(double dolar)
+        {
+            this.dolar = dolar;
+        }
+
+        public double getDolar(double dolar)
+        {
+            return this.dolar;
+        }
+
+        public void setKm(double real)
+        {
+            this.real = real;
+        }
+
+
+
+        public double getReal()
+        {
+            return this.real;
+        }
+
+        public void calcularReal()
+        {
+            this.real = this.dolar * 5.44;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ projPOO_ex7/projPOO_ex7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dolar dol;
+            dol = new Dolar();
+
+            Console.WriteLine("Digite o valor em dólares: ");
+            dol.setDolar(double.Parse(Console.ReadLine()));
+            dol.calcularReal();
+            Console.WriteLine("Valor em reais {0}.", dol.getReal());
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
