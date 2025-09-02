f3f842b1e3559ac67b9321f15fa4870cb261d505
nicollas <nicollas.b@aluno.ifsp.edu.br>
2025-09-01 21:06:55 -0300

    Add files via upload

---
+++ projPOO_ex9/projPOO_ex9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Maior2 ma;
+            ma = new Maior2();
+
+
+            Console.WriteLine("Digite um valor:");
+            ma.setA(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite outro valor:");
+            ma.setB(int.Parse(Console.ReadLine()));
+            ma.calcularAB();
+            Console.WriteLine("{0} é o maior valor", ma.getMaior());
+
+
+
+
+
+        }
+    }
+}
