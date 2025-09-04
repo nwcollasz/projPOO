4cf1f4f358f0796c5950612f267bdad81440a83b
nicollas <nicollas.b@aluno.ifsp.edu.br>
2025-09-04 15:38:32 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    
+	{
+
+
+        private int a;
+        private int b;
+        private int area;
+        public void setA(int a)
+        {
+            this.a = a;
+        }
+
+        public int getA(int a)
+        {
+            return this.a;
+        }
+
+        public void setB(int b)
+        {
+            this.b = b;
+        }
+
+        public int getB()
+        {
+            return this.b;
+        }
+
+        public int getMaior()
+        {
+            return this.area;
+        }
+        public void calcularAB()
+        {
+            this.area = this.a * this.b;
+            
+            
+            if (this.area > 100)
+            {
+                Console.WriteLine("Terreno grande:");
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+        
+    
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Class1 ma;
+            ma = new Class1();
+
+
+            Console.WriteLine("Digite a aresta:");
+            ma.setA(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite :");
+            ma.setB(int.Parse(Console.ReadLine()));
+            ma.calcularAB();
+            Console.WriteLine("{0} é sua área", ma.getMaior());
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
