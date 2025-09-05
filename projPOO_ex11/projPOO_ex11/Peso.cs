251064a68dfb98a12be4375cce9d0345e3f1ee59
nicollas <nicollas.b@aluno.ifsp.edu.br>
2025-09-05 10:40:38 -0300

    Add files via upload

---
+++ projPOO_ex11/projPOO_ex11/Peso.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex11
+{
+    internal class Peso
+    {
+
+        private double p;
+        private double a;
+        private double relacao;
+        public void setP(double p)
+        {
+            this.p = p;
+        }
+
+        public void setA(double a)
+        {
+            this.a = a;
+        }
+
+        public double getA()
+        {
+            return this.a;
+        }
+
+        public double getP()
+        {
+            return this.p;
+        }
+
+        public double getRel()
+        {
+            return this.relacao;
+        }
+        public void calcularRel()
+        {
+            this.relacao = this.p / (this.a * this.a);
+
+
+            if (this.relacao < 20)
+            {
+                Console.WriteLine("Abaixo do peso:");
+
+            }
+            else if (this.relacao >= 25)
+            {
+                Console.WriteLine("Acima do peso:");
+            }
+             if (this.relacao >= 20 && this.relacao < 25)
+            {
+              Console.WriteLine("Peso ideal");
+            }
+
+        }
+
+
+    }
+}
---
+++ projPOO_ex11/projPOO_ex11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Peso p = new Peso();
+
+            Console.WriteLine("Digite seu peso:");
+            p.setP(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite sua altura :");
+            p.setA(double.Parse(Console.ReadLine()));
+            p.calcularRel();
+            Console.WriteLine(p.getRel());
+
+        }
+    }
+}
