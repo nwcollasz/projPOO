8a954bf95fd513cdb2658baebe71ef4a6f09c464
nicollas <nicollas.b@aluno.ifsp.edu.br>
2025-09-05 11:13:56 -0300

    Add files via upload

---
+++ projPOO_ex12/projPOO_ex12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo t = new Triangulo();
+
+            Console.Write("Insira o primeiro valor: ");
+            t.setA(int.Parse(Console.ReadLine()));
+
+            Console.Write("Insira o segundo valor: ");
+            t.setB(int.Parse(Console.ReadLine()));
+
+            Console.Write("Insira o terceiro valor: ");
+            t.setC(int.Parse(Console.ReadLine()));
+
+            t.calcularABC();
+
+            
+
+        }
+    }
+}
---
+++ projPOO_ex12/projPOO_ex12/Triangulo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex12
+{
+    internal class Triangulo
+    {
+        private int a;
+        private int b;
+        private int c;
+        private int tr;
+
+        public void setA(int a)
+        {
+            this.a = a;
+        }
+
+        public int getA(int a)
+        {
+            return this.a;
+        }
+
+        public void setB(int b)
+        {
+            this.b = b;
+        }
+
+        public int getB()
+        {
+            return this.b;
+        }
+
+        public void setC(int c)
+        {
+            this.c = c;
+        }
+
+        public int getC()
+        {
+            return this.c;
+        }
+
+        public void setTr(int tr)
+        {
+            this.tr = tr;
+        }
+
+        public int getTr(int tr)
+        {
+            return this.tr;
+        }
+
+
+
+        public void calcularABC()
+        {
+
+            if (a + b > c && a + c > b && c + b > c)
+            {
+                
+                Console.Write("Forma triangulo.");
+            }
+            else
+            {
+                Console.Write("Nao forma triangulo.");
+            }
+
+
+            if (a == c && a == b && b == c)
+            {
+                Console.Write(" E sua classificacao e 'Equilatero'.");
+            }
+            else
+            {
+                if (a == c || c == b || b == a)
+                    Console.WriteLine(" E sua classificacao e 'Isoceles'.");
+            }
+            if (a != c && a != b && b != c)
+            {
+                Console.WriteLine(" E sua classificacao e 'Escaleno");
+            }
+        }
+
+
+
+    }
+    
+    
+}
+
+
