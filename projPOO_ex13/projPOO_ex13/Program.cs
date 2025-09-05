10b1590126d7b5698e3ec1e880da5a379a6172b1
nicollas <nicollas.b@aluno.ifsp.edu.br>
2025-09-05 11:21:11 -0300

    Add files via upload

---
+++ projPOO_ex13/projPOO_ex13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo2 t = new Triangulo2();
+
+            Console.Write("Insira o primeiro valor: ");
+            t.setA(int.Parse(Console.ReadLine()));
+
+            Console.Write("Insira o segundo valor: ");
+            t.setB(int.Parse(Console.ReadLine()));
+
+            Console.Write("Insira o terceiro valor: ");
+            t.setC(int.Parse(Console.ReadLine()));
+
+            t.calcularABC();
+
+        }
+    }
+}
---
+++ projPOO_ex13/projPOO_ex13/Triangulo2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex13
+{
+    internal class Triangulo2
+    {
+        private int a;
+        private int b;
+        private int c;
+
+        public void setA(int a)
+        {
+            this.a = a;
+        }
+
+        public int getA(int a)
+        {
+            return this.a;
+        }
+
+        public void setB(int b)
+        {
+            this.b = b;
+        }
+
+        public int getB()
+        {
+            return this.b;
+        }
+
+        public void setC(int c)
+        {
+            this.c = c;
+        }
+
+        public int getC()
+        {
+            return this.c;
+        }
+
+        public void calcularABC()
+        {
+            if ((c * c) == (a * a) + (b * b) || (b * b) == (a * a) + (c * c) || (a * a) == (c * c) + (b * b))
+            {
+                Console.Write("Forma triangulo retangulo.");
+
+            }
+            else
+            {
+                Console.Write("Nao forma triangulo retangulo.");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
